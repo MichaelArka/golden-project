cef7c1d7604c67e1eec4266697cff6747afee536
Michal Andronik <michal.androni@gmail.com>
2021-08-16 18:45:46 +0200

    fix nav/buger

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
-const menuBtn = document.querySelector('.menu-btn');
-let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-    if(!menuOpen) {
-        menuBtn.classList.add('open');
-        menuOpen = true;
-    } else {
-        menuBtn.classList.remove('open');
-        menuOpen = false;
-    }
-});
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li'); 
+
+    burger.addEventListener('click', () => {
+            
+        //toggle nav
+        nav.classList.toggle('nav-active');
+
+        //animate links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.0}s`;
+            }
+        });
+    });
+}
+navSlide();
